a65741591c890f07d0b41dcc93f8652235798cc6
Two_Flower <37899663+huaqiangbeijiedao@users.noreply.github.com>
2019-05-09 21:30:15 +0200

    Add project files.

---
+++ KursMVC/Controllers/MoviesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.VieweModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var cutomers = new List<Customer>() {new Customer {Name="Piotr"},
+                new Customer {Name="Monika"},
+            };
+            var viewModel = new RandomMovieViewModel { Customers = cutomers, Movie = movie };
+            return View(viewModel);
+        }
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("page insex={0} and sortby={1}", pageIndex, sortBy));
+        }
+        public ActionResult Edit (int id)
+        {
+            return Content("id=" + id);
+        }
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ KursMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KursMVC.Startup))]
+namespace KursMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
